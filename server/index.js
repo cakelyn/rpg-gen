3ab7db35c3bd21a2397a5bdb843706d17586a411
Katelyn Stamas <kstamas@saftlaunch.com>
2018-08-03 15:42:35 -0400

    set up server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+app.set("port", process.env.PORT || 1337);
+const port = app.get("port");
+app.use(express.static(path.join(__dirname, "/../client")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(port, () => console.log(`listening on port ${port}`));
